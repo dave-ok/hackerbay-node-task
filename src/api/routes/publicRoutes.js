821763162e5e7b43a085fa4595e3ea8191cd8758
Dave <callremz@gmail.com>
2021-02-02 16:25:27 +0100

    add publi login route

--- src/api/routes/publicRoutes.js
+++ src/api/routes/publicRoutes.js
@@ -1,5 +1,8 @@
 import express from "express";
+import { publicCtrl } from "../controllers/publicController";
 
 const publicRoutes = express.Router();
+
+publicRoutes.post("/", publicCtrl.login);
 
 export default publicRoutes;
